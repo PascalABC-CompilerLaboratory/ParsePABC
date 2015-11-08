2addcdd06dadcea113410e5f9beb2f62af2ca4e4
Oleg Batashov <krash.se.us@gmail.com>
2015-11-09 00:13:00 +0300

    Self-replacing formal method parameters visitor refactor

--- SyntaxVisitors/YieldVisitors/ReplaceFormalParametersRefsVisitor.cs
+++ SyntaxVisitors/YieldVisitors/ReplaceFormalParametersRefsVisitor.cs
@@ -11,9 +11,6 @@ namespace SyntaxVisitors
 {
     public class ReplaceFormalParametersRefsVisitor : BaseChangeVisitor
     {
-        // Для вложенных функций
-        private List<HashSet<string>> formalParameters = new List<HashSet<string>>();
-
         public HashSet<string> CollectedFormalParameters = new HashSet<string>();
 
         // Типа-стек соответствий paramName -> <>num__paramName для разных уровней вложенности методов
@@ -21,7 +18,6 @@ namespace SyntaxVisitors
 
         public ReplaceFormalParametersRefsVisitor()
         {
-            //this.formalParameters = new HashSet<string>(formalParameters);
         }
 
         public override void visit(procedure_definition pd)
@@ -31,8 +27,6 @@ namespace SyntaxVisitors
                 base.visit(pd.proc_body);
                 return;
             }
-
-            //formalParameters.Add(new HashSet<string>());
             formalParametersStack.Add(new Dictionary<string, string>());
             int currentLevel = formalParametersStack.Count - 1;
 
@@ -40,21 +34,17 @@ namespace SyntaxVisitors
             {
                 foreach (var id in plist.idents.idents)
                 {
-                    //formalParameters[formalParameters.Count - 1].Add(id.name);
-
                     var paramName = id.name;
                     var hoistedParamName = HoistParametersHelper.MakeHoistedFormalParameterName(id.name);
 
                     formalParametersStack[currentLevel].Add(paramName, hoistedParamName);
 
+                    // Захват
                     CollectedFormalParameters.Add(hoistedParamName);
-
                 }
             }
 
             base.visit(pd.proc_body);
-
-            //formalParameters.RemoveAt(formalParameters.Count - 1);
 
             formalParametersStack.RemoveAt(currentLevel);
         }
@@ -77,8 +67,6 @@ namespace SyntaxVisitors
                 }
             }
 
-            //if (formalParameters.Select(fp => fp.Contains(id.name)).Contains(true))
-
             if ((object)paramNameLevel != null)
             {
                 var upper = UpperNode();
@@ -91,8 +79,6 @@ namespace SyntaxVisitors
                     var self = new ident("self", id.source_context);
 
                     // Заменяем paramName -> self.hoistedParamName: <>num__paramName
-
-                    //var selfId = new dot_node(self, id);
 
                     var hoistedParamName = new ident(formalParametersStack[(int)paramNameLevel][paramName], id.source_context);
 
